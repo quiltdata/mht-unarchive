8091e9f2daf332641901fa125596ac7fcd44e548
Ernest Prabhakar <ernest@quiltdata.io>
2023-01-18 15:18:25 -0800

    fix file extensions

--- extract.py
+++ extract.py
@@ -4,6 +4,7 @@ import logging
 import quopri
 import sys
 from pathlib import Path
+from urllib.parse import urlparse
 
 from email import message_from_file
 
@@ -16,31 +17,57 @@ def unquote(quoted):
     content = decoded.decode('utf-8')
     return content
 
+def extract_file_ext(file_name):
+    return Path(file_name).suffix.replace('.','')
+
+def extract_filename(file_path, ctype):
+    file_name = file_path.name  # myfile.png
+    ext = extract_file_ext(file_name)
+    if ctype[-1] == 'svg+xml': 
+        ctype.append('svg')
+
+    if ext in ctype: 
+        return file_name 
+
+    split = file_name.split('@')
+    if split[-1] == 'mhtml.blink':
+        return f'{split[-0]}.css'
+
+    return f'{file_name}.{ctype[-1]}'
+
 class Extract():
     def __init__(self, source_file):
         with open(source_file, 'r') as f:
             self.msg = message_from_file(f)
 
         self.html = None
-        self.files = {}
+        self.attrs = {}
+        self.payloads = {}
         for part in self.msg.walk():
             if not part.is_multipart():
                 self.parse_part(part)
 
+    def add_file(self, uri, ctype):
+        sections = urlparse(uri)
+        file_path = Path(sections.path)
+        file_name = extract_filename(file_path, ctype)
+        file_ext = extract_file_ext(file_name)
+
+        attrs = {
+            "uri": uri,
+            "uri.sections": sections,
+            "path": file_path,
+            "name": file_name,
+            "ext": file_ext,
+        }
+        self.attrs[file_name] = attrs
+        return attrs
+
     def parse_part(self, part):
-        ctype = part.get('Content-Type')
+        ctype = part.get('Content-Type').split('/')
         quoted = part.get('Content-Type') == 'quoted-printable'
-        url = part.get('Content-Location')
+        uri = part.get('Content-Location')
         payload = part.get_payload()
-
-        file_path = Path(url)
-        file_name = Path(file_path).name  # myfile.png
-        file_stem = Path(file_path).stem  # myfile
-        if file_stem not in ctype:
-            print(f'ctype {ctype}')
-            print(f'url {url}')
-            print(f'file_name {file_name}')
-
         if quoted:
             payload = unquote(payload)
 
@@ -48,7 +75,9 @@ class Extract():
             assert None == self.html
             self.html = payload
         else:
-            self.files[url] = payload
+            attrs = self.add_file(uri, ctype)
+            self.payloads[attrs["name"]] = payload
+            logging.debug(f'file_name {attrs["name"]}')
 
     def extra_text(self):
         print(self.msg.preamble)
--- test_extract.py
+++ test_extract.py
@@ -4,3 +4,14 @@ TEST_FILE='sample.mht'
 def test_extract():
 	ex = Extract(TEST_FILE)
 	assert ex
+	assert ex.html
+	assert 'Brandon F' in ex.html
+	assert ex.attrs
+	keys = list(ex.attrs.keys())
+	k1 = keys[0]
+	assert 'css' in k1
+
+	attrs = ex.attrs.get(k1)
+	assert attrs
+	#print(attrs)
+	#assert False
