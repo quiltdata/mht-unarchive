04ebb510d4661293cc64294ef49dff07f7787749
Ernest Prabhakar <ernest@quiltdata.io>
2023-01-18 15:22:21 -0800

    test extract_filename

--- test_extract.py
+++ test_extract.py
@@ -1,4 +1,6 @@
 from extract import Extract
+from pathlib import Path
+
 TEST_FILE='sample.mht'
 
 def test_extract():
@@ -8,10 +10,15 @@ def test_extract():
 	assert 'Brandon F' in ex.html
 	assert ex.attrs
 	keys = list(ex.attrs.keys())
-	k1 = keys[0]
-	assert 'css' in k1
+	file_name = keys[0]
+	assert 'css' in file_name
 
-	attrs = ex.attrs.get(k1)
+	attrs = ex.attrs.get(file_name)
 	assert attrs
-	#print(attrs)
-	#assert False
+	assert '@mhtml.blink' in attrs['uri']
+	assert '@mhtml.blink' not in file_name
+	assert '.css' in file_name
+
+	for key in keys:
+		suffix = Path(key).suffix
+		assert suffix # every filename has a suffix
